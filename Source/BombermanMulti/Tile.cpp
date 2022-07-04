2f839c0c921cd65e2803026ff291053778fb2db1
LucasKyinn <LucasKyinn@gmail.com>
2022-07-04 18:16:42 +0200

    Had to make a little segway
    -Added Tile Class
    -Added Brick Class
    -Set up Tile BP
    -Made 2 reallyBasic Green Material to alternate colors when building the Arena

---
+++ Source/BombermanMulti/Tile.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tile.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "Brick.h"
+#include <Components/BoxComponent.h>
+
+
+ATile::ATile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	if (!ensure(RootScene != nullptr)) return;
+	RootComponent = RootScene;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	if (!ensure(MeshComp != nullptr)) return;
+	MeshComp->SetupAttachment(RootComponent);
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision Box"));
+	CollisionBox->SetupAttachment(RootComponent);
+	if (!ensure(CollisionBox != nullptr)) return;
+	CollisionBox->SetupAttachment(MeshComp);
+
+	TileType = 0;
+	MatType = 0;
+}
+
+ATile::ATile(int OTileType,int OMatType) : ATile()
+{
+	MatType = OMatType;
+	TileType = OTileType;
+}
+
+// Called when the game starts or when spawned
+void ATile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (TileType != 0) {
+		CollisionBox->Deactivate();
+
+		if (UnbreakabkeBrick != nullptr && Brick !=nullptr) {
+			UWorld* World = GetWorld();
+			if (World != nullptr) {
+				FActorSpawnParameters SpawnParams;
+				SpawnParams.Owner = this;
+				FRotator Rotator;
+				if (TileType == 1) {
+					FVector Location = GetActorLocation();
+
+					World->SpawnActor<ABrick>(UnbreakabkeBrick, Location, Rotator, SpawnParams);
+				}
+				if (TileType == 2) {
+					FVector Location = GetActorLocation()+FVector(0.f,0.f,20.f);
+
+					World->SpawnActor<ABrick>(Brick, Location, Rotator, SpawnParams);
+				}
+			}
+		}
+	}
+	if (FirstMat != nullptr && SecondMat != nullptr) {
+		if (MatType == 0) MeshComp->SetMaterial(0, FirstMat);
+		if (MatType == 1) MeshComp->SetMaterial(0, SecondMat);
+	}
+}
+
+// Called every frame
+void ATile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BombermanMulti/Tile.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class BOMBERMANMULTI_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	ATile(int OTileType, int OMatType);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, Category = BreakableBlock, meta = (AllowPrivateAccess = "true"))
+	class USceneComponent* RootScene;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UMaterialInterface* FirstMat;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UMaterialInterface* SecondMat;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"));
+	class UBoxComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere , Category = "Components")
+	TSubclassOf<class ABrick> UnbreakabkeBrick;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	TSubclassOf<class ABrick> Brick;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Components")
+	int MatType=0 ;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	int TileType = 0;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
